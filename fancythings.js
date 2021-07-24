4c200cb60d630071e7877de217e42a51ce35481d
Cryostat <59984625+Cryostat@users.noreply.github.com>
2021-07-23 22:06:52 -0400

    Update fancythings.js

---
+++ fancythings.js
@@ -0,0 +1,16 @@
+function mobileMenu() {
+    var node = document.getElementById("sidenav");
+    document.getElementById("menu-display").appendChild(node);
+    toggle();
+  }
+  
+  function toggle() {
+    var x = document.getElementById("menu-display");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }
+    else {
+      x.style.display = "block";
+    }
+  }
+  document.getElementById("menu-link").addEventListener("click", mobileMenu);
