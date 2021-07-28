7387eb31277fe5ebdc1bf0d783b9cadacf318deb
Cryostat <59984625+Cryostat@users.noreply.github.com>
2021-07-27 23:24:50 -0400

    Javascript Button Addition

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-function menuPancake() {
-    var item = document.getElementById("sidemenu");
-    document.getElementById("menu-display").appendChild(node);
-    toggle();
-}
-function toggle() {
-    var x = document.getElementById("menu-display");
-    if (x.style.dispay === "block") {
-        x.style.display = "none";    
+//Simple JS calls functions//
+
+var menu = document.querySelector(".menu");
+var pancake = document.querySelector(".pancake");
+var exitIcon = document.querySelector(".exitIcon");
+var menuIcon = document.querySelector(".menuIcon");
+
+pancake.addEventListener("click", toggleMenu);
+menu.classList.contains("showMenu");
+
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+      menu.classList.remove("showMenu");
+      exitIcon.style.display = "none";
+      menuIcon.style.display = "block";
+    } else {
+      menu.classList.add("showMenu");
+      exitIcon.style.display = "block";
+      menuIcon.style.display = "none";
     }
-    else {
-        x.style.display = "block";
+  }
+
+  var menuLinks = document.querySelectorAll(".menuLink");
+
+  menuLinks.forEach( 
+    function(menuLink) { 
+      menuLink.addEventListener("click", toggleMenu);
     }
-}
-document.getElementById("menu").addEventListener("click", menuPancake);
+  )
+  
