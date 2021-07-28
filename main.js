4ef3f3c330ac75a9e749697bd0e6cc3674e86799
Cryostat <59984625+Cryostat@users.noreply.github.com>
2021-07-27 21:02:19 -0400

    button update

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 function menuPancake() {
     var item = document.getElementById("sidemenu");
+    document.getElementById("menu-display").appendChild(item);
+    toggle();
 }
+function toggle() {
+    var x = document.getElementById("menu-dispay");
+    if (x.style.dispay === "block") {
+        x.style.display = "none";    
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+document.getElementById("menu").addEventListener("click", mobileMenu);
